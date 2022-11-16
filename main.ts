9fe6805fae490421f18877172748d18daf6133be
coraliefilion <116817945+coraliefilion@users.noreply.github.com>
2022-11-16 14:14:50 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,62 @@
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    if (distance == 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (distance == 8) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
+    if (distance == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (distance == 5) {
+        basic.showLeds(`
+            # . . . #
+            . # # # .
+            . # # # .
+            . # # # .
+            # . . . #
+            `)
+    }
+    if (distance == 4) {
+        basic.showLeds(`
+            # # . # #
+            # . # . #
+            . # # # .
+            # . # . #
+            # # . # #
+            `)
+    }
+    if (distance == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
